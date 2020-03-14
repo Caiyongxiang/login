08c9a883cb0a71c590905143a1d5a3a1c17ff770
caiyongxiang <1796132286@qq.com>
2020-03-14 15:24:14 +0800

    123

--- rem.js
+++ rem.js
@@ -6,8 +6,40 @@
     var id = null;
     function setRem() {
         id = new Date().getTime();
-
-        var fontSize = innerWidth >= 768 ? '100px' : innerWidth / 767 * 100 + 'px';
+         if(innerWidth<=320){
+             var fontSize = 50 + 'px';
+         }else if(innerWidth<=360){
+            var fontSize = 56.26  + 'px';
+         }else if(innerWidth<=375){
+            var fontSize = 58.29375  + 'px';
+         }else if(innerWidth<=400){
+            var fontSize =  62.5 + 'px';
+         }else if(innerWidth<=414){
+            var fontSize = 64.6875  + 'px';
+         }else if(innerWidth<=440){
+            var fontSize = 68.75  + 'px';
+         }else if(innerWidth<=480){
+            var fontSize = 75  + 'px';
+         }else if(innerWidth<=520){
+            var fontSize = 81.75  + 'px';
+         }else if(innerWidth<=560){
+            var fontSize =   87.5+ 'px';
+         }else if(innerWidth<=600){
+            var fontSize =   93.75+ 'px';
+         }else if(innerWidth<=640){
+            var fontSize =  100+ 'px';
+         }else if(innerWidth<=680){
+            var fontSize =   106.25+ 'px';
+         }else if(innerWidth<=720){
+            var fontSize =   112.5+ 'px';
+         }else if(innerWidth<=760){
+            var fontSize =   118.75+ 'px';
+         }else if(innerWidth<=800){
+            var fontSize =  125+ 'px';
+         }else if(innerWidth<=960){
+            var fontSize =  150+ 'px';
+         }
+        // var fontSize = innerWidth >= 768 ? '100px' : innerWidth / 767 * 100 + 'px';
 
         var style = $('<style>html{font-size: ' + fontSize + ';}</style>');
         style.attr('id', id);
